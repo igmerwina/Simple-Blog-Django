c23891f69548c140c8964d6d392b54239bb5ea45
I Gusti Made Erwin Ardiantha <23517034@std.stei.itb.ac.id>
2018-03-14 20:21:01 +0700

    git commit -m 'forms and user accounts'

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', # new
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # setting untuk static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
