cec8a7e9a6c0e11d9c637661ac803c5fc133c06f
I Gusti Made Erwin Ardiantha <23517034@std.stei.itb.ac.id>
2018-03-14 19:59:26 +0700

    Added CRUD Function

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 # blog/models.py
 from django.db import models
+from django.urls import reverse 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,6 +9,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+# blog/views.py
 from django.views.generic import ListView, DetailView
-
-from . models import Post 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . models import Post
 
 class BlogListView(ListView):
   model = Post 
@@ -10,3 +12,18 @@ class BlogDetailView(DetailView):
   model = Post 
   template_name = 'post_detail.html'
   context_object_name = 'anything_you_want'
+
+class BlogCreateView(CreateView):
+  model = Post 
+  template_name = 'post_new.html'
+  fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+class BlogDeleteView(DeleteView):
+  model = Post
+  template_name = 'post_delete.html'
+  success_url = reverse_lazy('home')
